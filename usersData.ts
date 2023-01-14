21dccc39ef3125908a320d679f78870e37ff2031
Shmeb <1gledpobgornuk@gmail.com>
2023-01-14 18:22:32 +0300

    rn-swipper

---
+++ usersData.ts
@@ -0,0 +1,16 @@
+export interface UserModel {
+    id: number;
+    avatar: string;
+  }
+  
+  export default [
+    {
+      id: 0,
+      fio: 'Ana Marija Gallusova',
+      post: 'Designer',
+      descr: 'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.',
+      avatar: require('./assets/slideUserPic.png'),
+      portF: require('./assets/port-1.png'),
+      portS: require('./assets/port-2.png'),
+    },
+  ];
